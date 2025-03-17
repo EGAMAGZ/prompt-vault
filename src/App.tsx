7477e5a661eb7ba5d2fad8abe77aa858f76fb355
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-03-17 12:10:50 -0600

    refactor: Usage of Deno

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    	<h1 class="text-8xl">Sample</h1>
+      <h1 className="text-8xl">Sample</h1>
       <Button>Default</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
