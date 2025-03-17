e8d340e95d481de94656e89d8ca60e2ca5b96ce5
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-03-17 13:26:36 -0600

    feat: Add initial Electron app structure and assets

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.egamagz.prompvault',
+  appName: 'prompt-vault',
+  webDir: 'dist'
+};
+
+export default config;
